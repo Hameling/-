b2df4c121df7fd776f3e4401d375bcd03e3c1b80
juho1210 <wngh9512@gmail.com>
2019-04-05 17:09:51 +0900

    준덕이가 upload.php 변형하기 쉽게끔 html폼으로 맞춤

--- upload.php
+++ upload.php
@@ -1,4 +1,12 @@
-<form enctype='multipart/form-data' action='upload_ok.php' method='post'>
-	<input type='file' name='myfile'>
-	<button>보내기</button>
-</form>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title></title>
+	</head>
+	<body>
+		<form action="upload_ok.php" method="POST" enctype="multipart/form-data">
+			<input type='file' name='myfile'>
+			<input type="submit" value="보내기">
+		</form>
+	</body>
+</html>
