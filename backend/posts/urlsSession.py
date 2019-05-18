ceb602cda09b335da79c396c7339c7874aeb4747
juho1210 <wngh9512@gmail.com>
2019-05-18 15:22:44 +0900

    url 수정

--- backend/posts/urlsSession.py
+++ backend/posts/urlsSession.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.SessionList.as_view()),
-    path('create-setion/', views.SessionCreate.as_view()),
-    path('search-setion/<pk>/', views.SessionSearch.as_view()),
-    path('delete-setion/<pk>/', views.SessionDelete.as_view()),
+    path('create-session/', views.SessionCreate.as_view()),
+    path('search-session/<pk>/', views.SessionSearch.as_view()),
+    path('delete-session/<pk>/', views.SessionDelete.as_view()),
 ]
