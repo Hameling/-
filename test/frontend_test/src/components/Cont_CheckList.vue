8dc5faa7247a8b05b1e98cc3f822b889ce4e24f3
백승한 <hameli@naver.com>
2019-05-22 02:09:32 +0900

    서버 사이드 테스트용

--- test/frontend_test/src/components/Cont_CheckList.vue
+++ test/frontend_test/src/components/Cont_CheckList.vue
@@ -3,8 +3,8 @@
     <ul v-for="(checklist, i) in checklists" :key="i" class="list-unstyled">
       <li>
         {{checklist.listname}}
-        <span v-if="checklist.listnumber != null" v-on:click="delCheckList(checklist.listnumber)" class="close" aria-hidden="true">&times;</span>
-        <span v-else v-on:click="delCheckList(i)" class="close" aria-hidden="true">&times;</span>
+        <!--<span v-if="checklist.listnumber != null" v-on:click="delCheckList(checklist.listnumber)" class="close" aria-hidden="true">&times;</span>
+        <span v-else v-on:click="delCheckList(i)" class="close" aria-hidden="true">&times;</span>-->
       </li>
     </ul>
   </div>
