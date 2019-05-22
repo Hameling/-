ad750282d3542f02be0f7b0681d2417cfa8f3e7c
SeoLYNN <junkun699@gmail.com>
2019-05-22 15:32:41 +0900

    span 주석

--- test/frontend_test/src/components/Cont_Comment.vue
+++ test/frontend_test/src/components/Cont_Comment.vue
@@ -3,8 +3,8 @@
     <ul v-for="(comment, i) in comments" :key="i" class="list-unstyled">
       <li>
         {{comment.comnumber}}.{{comment.memberid}}: {{comment.comcomment}} [{{comment.commenttime}}]
-        <span v-if="comment != null" v-on:click="delComment(comcoment)" class="close" aria-hidden="true">&times;</span>
-        <span v-else v-on:click="delComment(i)" class="close" aria-hidden="true">&times;</span>
+        <!--<span v-if="comment != null" v-on:click="delComment(comcoment)" class="close" aria-hidden="true">&times;</span>
+        <span v-else v-on:click="delComment(i)" class="close" aria-hidden="true">&times;</span>-->
       </li>
     </ul>
   </div>
