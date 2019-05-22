c0587bfd605d21f8e8178a8bc144f1b421502df4
SeoLYNN <junkun699@gmail.com>
2019-05-22 19:35:19 +0900

    커어밋

--- test/frontend_test/src/components/ContentForm.vue
+++ test/frontend_test/src/components/ContentForm.vue
@@ -1,126 +1,110 @@
-<template>
-  <div class="container">
-    <div class="card card-content mx-auto 5grid-layout">
-      <div class="card-header">Content</div>
-      <div class="card-body 5grid-layout">
-        <div class="row">
-          <div class="7u">
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="text"
-                  id="ContentTitle"
-                  class="form-control"
-                  placeholder="Title"
-                  required="required"
-                  autofocus="autofocus"
-                >
-                <label for="ContentTitle">Content Title</label>
+  <template>
+  <div class="bg-dark">
+    <div class="container">
+      <div class="card card-content mx-auto 5grid-layout">
+        <div class="card-header">Content</div>
+        <div class="card-body 5grid-layout">
+          <div class="row">
+            <div class="7u" id="leftcontent">
+              <section>
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="ContentTitle"
+                    class="form-control"
+                    placeholder="Title"
+                    required="required"
+                    autofocus="autofocus"
+                  >
+                  <label for="ContentTitle">Content Title</label>
+                </div>
+              </section>
+              <div>
+                <br>
               </div>
-            </section>
-            <div>
-              <br>
+              <section>
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="Subject"
+                    class="form-control"
+                    placeholder="Subject"
+                    required="required"
+                    autofocus="autofocus"
+                  >
+                  <label for="Subject">Subject</label>
+                </div>
+              </section>
+              <div>
+                <br>
+              </div>
+              <section>
+                <div class="form-label-group">
+                  <input type="text" id="inputText" class="form-control-comment" placeholder="New Comment" required="required" autofocus="autofocus" v-model="cmt_content" v-on:keyup.enter="addComment(cmt_content)">
+                  <button id="btnAdd" class="btn btn-primary" v-on:click="addComment(cmt_content)">Add</button>
+                  <cComment v-bind:comments="comments" v-on:del-comment="delComment"> </cComment>
+                </div>
+              </section>
             </div>
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="text"
-                  id="Subject"
-                  class="form-control"
-                  placeholder="Subject"
-                  required="required"
-                  autofocus="autofocus"
-                >
-                <label for="Subject">Subject</label>
-              </div>
-            </section>
-            <div>
-              <br>
-            </div>
-            <section>
-              <div class="form-label-group">
-                <input type="text" id="inputText" v-model="cmt_content" v-on:keyup.enter="addComment(cmt_content)">
-                <button id="btnAdd" v-on:click="addComment(cmt_content)">Add</button>
-                <cComment v-bind:comments="comments" v-on:del-comment="delComment"> </cComment>
-              </div>
-            </section>
-          </div>
 
-          <div class="3u" id="sidebar1">
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="text"
-                  id="AssignedPerson"
-                  class="form-control"
-                  placeholder="AssignedPerson"
-                  required="required"
-                >
-                <label for="AssignedPerson">Assigned Person</label>
+            <div class="3u" id="sidebar1">
+              <section>
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="AssignedPerson"
+                    class="form-control"
+                    placeholder="AssignedPerson"
+                    required="required"
+                  >
+                  <label for="AssignedPerson">Assigned Person</label>
+                </div>
+              </section>
+              <section>
+                <div id="myDIV" class="header">
+                  <h2>쓰퀘에쥬울</h2>
+                  <input type="text" id="myInput" placeholder="Title..." v-model="ckl_content" v-on:keyup.enter="addCheckList(ckl_content)">
+                  <span onclick="newElement()" class="addBtn" v-on:click="addCheckList(ckl_content)">Add</span>
+                  <cChecklist v-bind:checklists="checklists" v-on:del-checklist="delCheckList"> </cChecklist>
+                </div>
+              </section>
+              <div>
+                <br>
               </div>
-            </section>
-            <div>
-              <br>
+              <section>
+                <div class="form-label-group">
+                  <input
+                    type="file"
+                    id="File"
+                    class="dropbox"
+                    placeholder="File"
+                    required="required"
+                    @change="upload($event.target.name, $event.target.files)"
+                    @drop="upload($event.target.name, $event.target.files)"
+                  >
+                  <h2>파일을 드래그해서 드랍해주세요.</h2>
+                </div>
+              </section>
+              <div>
+                <br>
+              </div>
+              <section>
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="UpdateDate"
+                    class="form-control"
+                    placeholder="UpdateDate"
+                    required="required"
+                  >
+                  <label for="UpdateDate">Update Date</label>
+                </div>
+              </section>
             </div>
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="text"
-                  id="Schedule"
-                  class="form-control"
-                  placeholder="Schedule"
-                  required="required"
-                >
-                <label for="Schedule">Schedule</label>
-              </div>
-            </section>
-            <div>
-              <br>
-            </div>
-            <section>
-              <div id="myDIV" class="header">
-                <h2>Schedule</h2>
-                <input type="text" id="myInput" placeholder="Title..." v-model="ckl_content" v-on:keyup.enter="addCheckList(ckl_content)">
-                <span onclick="newElement()" class="addBtn" v-on:click="addCheckList(ckl_content)">Add</span>
-                <cChecklist v-bind:checklists="checklists" v-on:del-checklist="delCheckList"> </cChecklist>
-              </div>
-            </section>
-            <div>
-              <br>
-            </div>
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="file"
-                  id="File"
-                  class="dropbox"
-                  placeholder="File"
-                  required="required"
-                  @change="upload($event.target.name, $event.target.files)"
-                  @drop="upload($event.target.name, $event.target.files)"
-                >
-                <h2>파일을 드래그해서 드랍해주세요.</h2>
-              </div>
-            </section>
-            <div>
-              <br>
-            </div>
-            <section>
-              <div class="form-label-group">
-                <input
-                  type="text"
-                  id="UpdateDate"
-                  class="form-control"
-                  placeholder="UpdateDate"
-                  required="required"
-                >
-                <label for="UpdateDate">Update Date</label>
-              </div>
-            </section>
           </div>
         </div>
+        <!--<a class="btn btn-primary btn-block" href="#">Confirm</a>-->
       </div>
-      <a class="btn btn-primary btn-block" href="#">Confirm</a>
     </div>
   </div>
 </template>
