1982fe210c2069b5ed01e5119f423a04e7e93d99
Hameling <jerjiment@naver.com>
2019-05-24 03:44:24 +0900

    섹션 생성 페이지 추가

--- test/frontend_test/src/components/Section.vue
+++ test/frontend_test/src/components/Section.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="row inner-row">
+    <!--Modal 선언부 -->
+    <b-modal
+      id="create-section"
+      title="Create Section"
+      centered
+      ok-only
+      ref = 'modal'
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Section Name"
+          label-for="name-input"
+          invalid-feedback="Name is required"
+        >
+          <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+        </b-form-group>
+      </form>
+    </b-modal>
+
+    <!--메인 코드 시작부-->
     <div class="col-md-2" id="sessionbar">
       <section>
-        <router-link to>
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-              <a href="#">Create New Session</a>
-            </li>
-          </ol>
-        </router-link>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a @click="$bvModal.show('create-section')">Create New Session</a>
+          </li>
+        </ol>
       </section>
     </div>
 
@@ -21,8 +43,7 @@
             </div>
           </div>
 
- 
-          <Content v-bind:contents='section.includeContent' v-on:del-content="delContent"> </Content>
+          <Content v-bind:contents="section.includeContent" v-on:del-content="delContent"></Content>
 
           <div class="card-footer text-white clearfix small z-1">
             <!--새 컨텐트 작성-->
@@ -42,24 +63,54 @@ import Content from "@/components/Content";
 export default {
   name: "Section",
   props: ["sections"],
-  data(){
-      return {
-        contents:[]
-      }
+  data() {
+    return {
+      name: "",
+      nameState: null,
+    };
   },
   methods: {
-      createContent(){
+    getSection() {
+      //기존과 조금 다르게 emit을 이용하여 모든 정보를 로드해야할듯
+    },
+    createContent(scname) {
+      this.$http.post('http://211.109.53.216:20000/comment/checkcomment/', {
+           titleid: "8", sectionname:scname
+          }).then((res) => {
+              this.getSection()
+          })
+    },
+    delContent() {},
 
-      },
-      delContent(){
-
+    //Modal 관련코드
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid ? "valid" : "invalid";
+      return valid;
+    },
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvt) {
+      bvModalEvt.preventDefault();
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
       }
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$refs.modal.hide();
+        createContent(this.name)
+      })
+    }
   },
-  mounted() {
-    
-  },
+  mounted() {},
   components: {
-      'Content' : Content
+    Content: Content
   }
 };
 </script>
