e03f8b150fe3233432b07a1e488bbd8ea283a9ba
Hameling <jerjiment@naver.com>
2019-06-08 18:15:33 +0900

    타이틀 셋팅 및 일부 수정

--- test/frontend_test/src/components/modal/TitleSetting.vue
+++ test/frontend_test/src/components/modal/TitleSetting.vue
@@ -1,81 +1,77 @@
 <template>
-    <b-modal id="TitleSetting" title="Title Setting" centered>
-        <b-tabs>
-            <b-tab title="Title Info">
-                <section class="box">
-              <div v-if="nameState" class="form-label-group">
-                <input
-                  type="text"
-                  v-model="contentname"
-                  id="ContentTitle"
-                  class="form-control"
-                  placeholder="Title"
-                  required="required"
-                  autofocus="autofocus"
-                  @keyup.enter="updateName"
-                >
-                <label for="ContentTitle">Content Title</label>
-              </div>
-              <div v-else class="form-label-group">
-                <label for="ContentTitle" @click="setNameState" class="form-control">
-                  <strong>{{contentname}}</strong>
-                </label>
-                <br>
-                <br>
-              </div>
-            </section>
+  <b-modal id="TitleSetting" title="Title Setting" centered @hidden="resetState">
+    <b-tabs>
+      <b-tab title="Title Info">
+        <section class="box">
+          <div v-if="nameState" class="form-label-group">
+            <input
+              type="text"
+              v-model="titlename"
+              id="TitleName"
+              class="form-control"
+              placeholder="Title"
+              required="required"
+              autofocus="autofocus"
+              @keyup.enter="updateName"
+            >
+            <label for="TitleName">Content Title</label>
+          </div>
+          <div v-else class="form-label-group">
+            <label for="TitleName" @click="setNameState" class="form-control">
+              <strong>{{titlename}}</strong>
+            </label>
+            <br>
+            <br>
+          </div>
+        </section>
 
-            <section class="box">
-              <div v-if="subjectState" class="form-label-group">
-                <input
-                  type="text"
-                  v-model="contentinfo"
-                  id="ContentInfo"
-                  class="form-control"
-                  placeholder="Subject"
-                  required="required"
-                  @keyup.enter="updateInfo"
-                >
-                <label for="ContentInfo">Content Description</label>
-              </div>
-              <div v-else class="form-label-group">
-                <label
-                  for="ContentTitle"
-                  @click="setSubjectState"
-                  class="form-control"
-                >{{contentinfo}}</label>
-                <br>
-                <br>
-              </div>
-            </section>
-            </b-tab>
+        <section class="box">
+          <div v-if="subjectState" class="form-label-group">
+            <input
+              type="text"
+              v-model="titleinfo"
+              id="TitleInfo"
+              class="form-control"
+              placeholder="Subject"
+              required="required"
+              @keyup.enter="updateInfo"
+            >
+            <label for="TitleInfo">Title Description</label>
+          </div>
+          <div v-else class="form-label-group">
+            <label for="TitleInfo" @click="setSubjectState" class="form-control">{{titleinfo}}</label>
+            <br>
+            <br>
+          </div>
+        </section>
+      </b-tab>
 
-            <b-tab title="Enroll">
-                <!--Enroll된 멤버 확인 및 삭제 : 기존 리스트 사용-->
-                <!--Enroll 시킬 대상 추가 : 셀렉터 활용-->
-            </b-tab>
-            <b-tab title="Title Delete">
-                
-            </b-tab>
-        </b-tabs>
-        
-    </b-modal>
+      <b-tab title="Enroll">
+        <!--Enroll된 멤버 확인 및 삭제 : 기존 리스트 사용-->
+        <!--Enroll 시킬 대상 추가 : 셀렉터 활용-->
+      </b-tab>
+      <b-tab title="Title Delete"></b-tab>
+    </b-tabs>
+  </b-modal>
 </template>
 
 <script>
 import Multiselect from "vue-multiselect";
 import "vue-multiselect/dist/vue-multiselect.min.css";
 export default {
-    name:"titlesetting",
-    data:()=>({
-        nameState :null,
-        subjectState: null
-    }),
-    props:["select_item"],
+  name: "titlesetting",
+  data: () => ({
+    nameState: null,
+    subjectState: null,
 
-    methods: {
-        //titleUpdate
-        setNameState() {
+    titlename:"",
+    titleinfo:""
+  }),
+  props: ["select_item"],
+
+  methods: {
+    //titleUpdate
+    setNameState() {
       this.nameState = !this.nameState;
     },
     setSubjectState() {
@@ -87,7 +83,7 @@ export default {
       this.setNameState();
     },
 
-    updateInfo(){
+    updateInfo() {
       this.updateTitle();
       this.setSubjectState();
     },
@@ -98,8 +94,8 @@ export default {
           .post("http://211.109.53.216:20000/content/update-content/", {
             contentid: sessionStorage.contentid,
             token: sessionStorage.accessToken,
-            contentname: this.contentname,
-            contentinfo: this.contentinfo,
+            titlename: this.titlename,
+            titleinfo: this.titleinfo,
             contentstate: "1"
           })
           .then(res => {
@@ -118,7 +114,6 @@ export default {
       this.nameState = false;
       this.subjectState = false;
     }
-    },
-
-}
+  }
+};
 </script>
