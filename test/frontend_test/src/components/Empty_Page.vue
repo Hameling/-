9d190df3ce928e2a896663b886a0ba4863bf7b82
SeoLYNN <junkun699@gmail.com>
2019-06-12 05:10:05 +0900

    메인화면 추가

--- test/frontend_test/src/components/Empty_Page.vue
+++ test/frontend_test/src/components/Empty_Page.vue
@@ -1,6 +1,8 @@
 <template>
 
     <div id="content-wrapper" style="background-color: #212529;">
+        <div class="empty-box" style="background-image:url('main.png'); background-size: 100% 100%;">
+        </div>
     </div>
 </template>
 
