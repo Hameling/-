c89161e4a43ccf552ed276a24f1910f3af839228
SeoLYNN <junkun699@gmail.com>
2019-06-19 00:37:41 +0900

    comment 삭제 수정

--- test/frontend_test/src/components/CommentList.vue
+++ test/frontend_test/src/components/CommentList.vue
@@ -2,22 +2,16 @@
   <div>
     <ul v-for="(comment, i) in comments" :key="i" class="list-unstyled">
       <!-- 내가 쓴 코멘트 일때 -->
-      <li v-if="comment.memberid == memberid" class="my-comment-border" v-bind:id="'mycomment'+i">
+      <li v-if="comment.memberid == memberid" class="my-comment-border" v-bind:id="'mycomment'+i" >
         {{comment.memberid}} : {{comment.comcomment}} [{{formattingTime(comment.commenttime)}}]
-        <span
-          v-on:click="delComment(comment.comnumber)"
-          class="close"
-          aria-hidden="true"
-        >&times;</span>
-
         <!-- popover 시작부분 -->
         <b-popover v-bind:target="'mycomment'+i">
           <div>
             <p>Comment를 삭제하시겠습니까?</p>
             <!-- 디자인 수정요망 -->
-            <div class="row">
-              <b-button class="col-md-5 col-md-offset-1" variant="primary">Ok</b-button>
-              <b-button class="col-md-5 col-md-offset-1" variant="danger">Cancel</b-button>
+            <div class="wrap">
+              <b-button class="comment-delbtn-ok" variant="primary" v-on:click="delComment(comment.comnumber)">Ok</b-button>
+              <b-button class="comment-delbtn-no" variant="danger">Cancel</b-button>
             </div>
           </div>
         </b-popover>
